af843f31a0d69b70520b654d277cc1d4505725b2
Mustafa Alnoori <alnoorimustafa@gmail.com>
2023-05-12 16:00:09 +0300

    edited plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,11 +5,8 @@ module.exports = ({ env }) => ({
       providerOptions: {
         account: env("STORAGE_ACCOUNT"),
         accountKey: env("STORAGE_ACCOUNT_KEY"), //either account key or sas token is enough to make authentication
-        sasToken: env("STORAGE_ACCOUNT_SAS_TOKEN"),
-        serviceBaseURL: env("STORAGE_URL"), // optional
         containerName: env("STORAGE_CONTAINER_NAME"),
         defaultPath: "assets",
-        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
       },
     },
   },
